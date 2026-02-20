290c37052104da0b28d51be08d1596740449a409
Sanyam Jain <sanyamj138@gmail.com>
2026-02-20 23:29:07 +0530

    Scafflold Backfround Color Change

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -6,6 +6,7 @@ void main() {
         home: Scaffold(
           appBar: AppBar(
             title: Text('I Am Rich'),
+            backgroundColor: Colors.blueGrey,
           ),
       ),
     ),
